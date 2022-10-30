378e4e16985d65c6054cea4bcc3343002f6aeac1
ciropompeu <ciro.pompeu@gmail.com>
2022-10-30 15:46:29 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerencialmento de informações")
 console.log("aplicação está se conectando ao bando de dados")
 // mais um console log de teste:
 console.log("o paulo fez esse commit sem você ver na aula!")
+console.log("enviando para o github")
